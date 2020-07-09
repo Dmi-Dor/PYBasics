94f446c799c2bc47deab6dfa779781c4b89f7a15
Dmi-Dor <67724841+Dmi-Dor@users.noreply.github.com>
2020-07-09 07:17:43 +0300

    4thlesson

---
+++ 1-4.py
@@ -0,0 +1,10 @@
+def income():
+    try:
+        time = float(input('Выработка в часах: '))
+        salary = int(input('Ставка: '))
+        bonus = int(input('Премия: '))
+        res = time * salary + bonus
+        print(f'заработная плата сотрудника  {res}')
+    except ValueError:
+        return print('Not a number')
+income()
